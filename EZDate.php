acc0e4c7f1306ecd465477480e2b25ca4539c50a
Kevin McGinty <atomic.frameworks@gmail.com>
2013-05-07 21:36:44 -0400

    formatting

--- EZDate.php
+++ EZDate.php
@@ -1,44 +1,44 @@
 <?php
-	/*
-							
-		   ##     #####		Copyright (c) - Kevin McGinty
-		 # _ #	 ###		
-		#   #	#     		AtomicFrameworks
-		
-	*/
-	class EZDate extends DateTime {
-		// MySQL DATETIME friendly format
-	    public static $format = 'Y-m-d H:i:s';
-	 
-	    public function __construct($time = null, DateTimeZone $timeZone = null)
-	    {
-	    	// Ensure we have timestamp and not string
-	    	if( isTimestamp($time) )
-			{
-				$time = date(self::$format,$time);
-			}
-	        // If no time zone set to default
-			if($timeZone === null)
-	        {
-	            $timeZone = new DateTimeZone(date_default_timezone_get());
-	        }
-	        // Construct DateTime with current time & time zone
-	        parent::__construct($time, $timeZone);
-	    }
-	 	
-	    public function __toString()
-	    {
-	    	// Return the DateTime formatted by our format prop and cast as string
-	        return (string)parent::format(self::$format);
-	    }
-	}
-	
-	// Return true if $timestamp is an int && 10 characters long && a valid php int
-	function isTimestamp($timestamp) {
-	    return ((int) $timestamp === $timestamp) 
-	        && ($timestamp <= PHP_INT_MAX)
-	        && ($timestamp >= ~PHP_INT_MAX)
-	        && (strlen($timestamp) === 10);
-	}
-	
+    /*
+                            
+           ##     #####     Copyright (c) - Kevin McGinty
+         # _ #   ###        
+        #   #   #           AtomicFrameworks
+        
+    */
+    class EZDate extends DateTime {
+        // MySQL DATETIME friendly format
+        public static $format = 'Y-m-d H:i:s';
+     
+        public function __construct($time = null, DateTimeZone $timeZone = null)
+        {
+            // Ensure we have timestamp and not string
+            if( isTimestamp($time) )
+            {
+                $time = date(self::$format,$time);
+            }
+            // If no time zone set to default
+            if($timeZone === null)
+            {
+                $timeZone = new DateTimeZone(date_default_timezone_get());
+            }
+            // Construct DateTime with current time & time zone
+            parent::__construct($time, $timeZone);
+        }
+        
+        public function __toString()
+        {
+            // Return the DateTime formatted by our format prop and cast as string
+            return (string)parent::format(self::$format);
+        }
+    }
+    
+    // Return true if $timestamp is an int && 10 characters long && a valid php int
+    function isTimestamp($timestamp) {
+        return ((int) $timestamp === $timestamp) 
+            && ($timestamp <= PHP_INT_MAX)
+            && ($timestamp >= ~PHP_INT_MAX)
+            && (strlen($timestamp) === 10);
+    }
+    
 ?>
